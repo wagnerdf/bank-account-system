e3e9404125599b75c55ba8abfff1b3fd513dbbee
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2026-02-07 21:04:28 -0300

    feat: configure PostgreSQL and create Usuario entity

---
+++ backend/src/main/java/com/wagnerdf/backend/model/Role.java
@@ -0,0 +1,6 @@
+package com.wagnerdf.backend.model;
+
+public enum Role {
+	USER,
+	ADMIN
+}
---
+++ backend/src/main/java/com/wagnerdf/backend/model/Usuario.java
@@ -0,0 +1,52 @@
+package com.wagnerdf.backend.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String email;
+	
+	@Column(nullable = false)
+	private String senha;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Role role;
+	
+	@Column(nullable = false)
+	private Boolean ativo = true;
+	
+	@CreationTimestamp
+	private LocalDateTime dataCriacao;
+
+}
