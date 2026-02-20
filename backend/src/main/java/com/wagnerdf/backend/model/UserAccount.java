efd628703bbfea83fd586c69e251b69703879860
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2026-02-20 13:47:57 -0300

    feat: add relationship between UserAccount and BankAccount

--- backend/src/main/java/com/wagnerdf/backend/model/UserAccount.java
+++ backend/src/main/java/com/wagnerdf/backend/model/UserAccount.java
@@ -1,6 +1,8 @@
 package com.wagnerdf.backend.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -10,9 +12,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -51,5 +55,8 @@ public class UserAccount {
     @CreationTimestamp
     @Column(name = "created_at", updatable = false, nullable = false)
     private LocalDateTime createdAt;
+    
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<BankAccount> accounts = new ArrayList<>();
 
 }
